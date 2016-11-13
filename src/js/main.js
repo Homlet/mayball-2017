cddb8e1a78a9e18053aa32a2d123f648703a6772
Homlet <samlhub@gmail.com>
2016-11-13 22:40:15 +0000

    Basic layout of ticketing section.

--- src/js/main.js
+++ src/js/main.js
@@ -23,7 +23,7 @@
         // Set up scroll-to targets.
         $("nav a").click(function () {
             var target = $(this).attr("data-anchor");
-            controller.scrollTo("section." + target);
+            controller.scrollTo("#" + target);
             $("#hamburger-nav").prop("checked", false).trigger("change");
             return false;
         });
@@ -34,6 +34,30 @@
                 $("nav ul").addClass("open");
             } else {
                 $("nav ul").removeClass("open");
+            }
+        });
+
+        // Set class toggles so the current section is highlighted.
+        scenes = [];
+        $("nav a[data-anchor]").each(function () {
+            var section = $(this).attr("data-anchor");
+            var length = $("#" + section).outerHeight();
+            var options = {
+                triggerElement: "#" + section,
+                triggerHook: 0.4,
+                duration: length
+            };
+            scene = new ScrollMagic.Scene(options)
+                .setClassToggle(this, "selected")
+                .addTo(controller);
+            scenes.push([scene, section]);
+        });
+
+        // Set up a window resize hook to reset some values above.
+        $(window).resize(function () {
+            for (var i = 0; i < scenes.length; i++) {
+                var pair = scenes[i];
+                pair[0].duration($("#" + pair[1]).outerHeight());
             }
         });
     }
